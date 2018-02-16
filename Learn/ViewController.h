b1d34c8b44566cdd1ca986af1faa16ccd841ec01
TeeEye <vinceeewang@163.com>
2018-02-16 21:37:23 +0800

    Initial Commit

---
+++ Learn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Learn
+//
+//  Created by Vince on 2018/2/16.
+//  Copyright © 2018年 ChenghaoWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Learn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Learn
+//
+//  Created by Vince on 2018/2/16.
+//  Copyright © 2018年 ChenghaoWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
